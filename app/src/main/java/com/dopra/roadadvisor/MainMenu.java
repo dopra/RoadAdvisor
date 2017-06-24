0689cb4f17bf207a0fd5c88e5a0320c929706957
Pablo Prado <padanprado@gmail.com>
2017-06-24 16:31:36 -0500

    RoadAdvisor User v1.0

--- app/src/main/java/com/dopra/roadadvisor/MainMenu.java
+++ app/src/main/java/com/dopra/roadadvisor/MainMenu.java
@@ -2,7 +2,6 @@ package com.dopra.roadadvisor;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
@@ -11,20 +10,12 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.analytics.FirebaseAnalytics;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
-import com.google.firebase.remoteconfig.FirebaseRemoteConfig;
-import com.google.firebase.remoteconfig.FirebaseRemoteConfigSettings;
-
-import java.util.HashMap;
 
 public class MainMenu extends AppCompatActivity {
 
@@ -185,6 +176,12 @@ public class MainMenu extends AppCompatActivity {
                 cdc_roadConditionRef.setValue("CERRADO");
             }
         });
+
+        //Hide the Buttons (this is the main diference between Admin and Users
+        cdc_open_button.setVisibility(View.INVISIBLE);
+        cdc_closed_button.setVisibility(View.INVISIBLE);
+        cac_open_button.setVisibility(View.INVISIBLE);
+        cac_closed_button.setVisibility(View.INVISIBLE);
 
     }
 
